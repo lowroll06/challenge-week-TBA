c9566d4f39c7f120accaf2d90109f6077c4839a6
lowroll06 <momaar.jo234@gmail.com>
2024-09-30 15:11:56 +0200

    official TBA of day 1

--- challenge/game.py
+++ challenge/game.py
@@ -7,21 +7,24 @@ def start_game():
     while hp > 0:
         print(f"\n het is dag nummer {day} en je hebt {hp} HP")
         choice = input("\n je kijkt om je heen en hebt al een tentje. naar wat zoek je 1.hout of 2.eten?: ")
-        if choice <= "1":
+        if choice == "1":
             print("je hebt hout gevonden en rust de rest van de dag")
         elif choice == "2":
-            gather_food(hp)
+            hp = gather_food(hp)
         else:
             print("Typ 1 of 2 alsjeblieft")
-            if hp == 0:
-                print("hp is op")
-                ask = input("opnieuw proberen? ")
-                if ask.lower() == "ja":
-                    start_game()
-                else:
-                    print("bedankt voor het spelen")
-                    break
-            day+= 1
+
+        if hp <= 0:
+            print("hp is op")
+            ask = input("opnieuw proberen? ")
+            if ask.lower() == "ja":
+                start_game()
+            else:
+                print("bedankt voor het spelen")
+                break
+            
+    day+= 1
+                
 
 
 
@@ -35,9 +38,10 @@ def gather_food(hp):
     elif choice_2 == "2":
        choice_3 = input("maak je 1. een wapen of 2.jaag je met handen: ")
        if choice_3 =="1":
-           print("je maakt een wapen en zoekt gelijk naar prooi \n je komt een beer te gemoet en breekt je wapen tijdens het gevecht en gaat dood -100hp")
+           print("je maakt een wapen en zoekt gelijk naar prooi \n je komt een beer te gemoet en breekt je wapen tijdens het gevecht en gaat dood -100")
            hp -= 100
        elif choice_3 == "2":
            print("je besluit om met je handen te jagen en vangt een vis")
+    return hp
            
 start_game()
